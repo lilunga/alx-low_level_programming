58bdcb2c236f27b4c7914c497aa407a00101e5c0
lilunga <lys.m.ilunga@gmail.com>
2022-09-13 07:26:13 -0700

    printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+/**
+ * main - print the string in the printf function
+ * 
+ * This program will print 'with proper grammar, but the outcome is a piece of art'
+ * Return: 0
+ */
+
+int main (void)
+{
+	printf('with proper grammar, but the outcome is a piece of art,\n');
+	return (0);
+
+}
