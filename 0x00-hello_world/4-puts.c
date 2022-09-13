1b4d832ba5f8edeac0d145958bdcdd6a53ea1c1f
lilunga <lys.m.ilunga@gmail.com>
2022-09-13 07:14:22 -0700

    puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - will print the string in the put function
+ *
+ * This programme will print 'Programming us like building a multilingual puzzle'
+ * Return: 0
+ */
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
